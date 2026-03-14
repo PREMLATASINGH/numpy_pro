eb48cc0c7d08b7e2268f5c468dd34e29e53c9ee4
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-14 13:59:25 -0400

    plot

---
+++ pro3.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+days=np.array([1,2,3,4,5,6,7])
+temperature=np.array([30,40,50,60,70,80,90])
+plt.plot(days, temperature)
+plt.xlabel('Days')
+plt.ylabel('Temperature')
+plt.title('Temperature over Days')
+plt.show()
